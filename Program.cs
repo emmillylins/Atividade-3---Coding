223962562175948e2d5a273902b385efd078537a
emmy <104332292+emmillylins@users.noreply.github.com>
2022-10-24 23:15:37 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
-﻿class Program {
+class Program {
   public static void Main (string[] args) {
-    Console.WriteLine("Hello World");
+    Console.WriteLine("Hello Raoni, welcome to my game!!");
+
+    //objetos
+    arqueiro arqueiro = new arqueiro();
+    guerreiro guerreiro = new guerreiro();
+    feiticeiro feiticeiro = new feiticeiro();
+    espadachim espadachim = new espadachim();
+
+    guerreiro.mostraPersonagem();
+    arqueiro.mostraPersonagem();
+    feiticeiro.mostraPersonagem();
+    espadachim.mostraPersonagem();
+
   }
 }
